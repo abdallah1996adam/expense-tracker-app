e5019aa24aff789a8395757f92788bc0ae04f47e
abdallah1996adam <abdallahaadam@protonmail.com>
2021-10-11 17:00:13 +0200

    :rocket:

--- src/components/addForm/AddForm.jsx
+++ src/components/addForm/AddForm.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 //icons
 import { MdKeyboardArrowDown } from "react-icons/md";
 
+import { categories } from "../../constants";
+
 //scss
 import "./addform.css";
 
@@ -9,6 +11,7 @@ export const AddForm = () => {
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState("");
   const [category, setCategory] = useState(null);
+  const [isOpen, setIsOpen] = useState(false)
 
   const handleAmount = (e) => {
     const value = parseFloat(e.target.value);
@@ -40,10 +43,20 @@ export const AddForm = () => {
       </section>
       <section className="category-container">
         <div className="category">
-          <div>
+          <div className="category-dropdown">
             <label>Category</label>
             <MdKeyboardArrowDown />
           </div>
+          {isOpen && (
+              <div className='category-wrapper'>
+                  {categories.map((index,cat)=>(
+                      <div key={index} className="categ-item">
+                          <label >{cat.title}</label>
+                          <img src={cat.icon} alt={cat.title} />
+                           </div>
+                  ))}
+              </div>
+          )}
         </div>
       </section>
     </main>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,38 @@
+import { FaGraduationCap } from "react-icons/fa";
+import { RiMentalHealthFill } from "react-icons/ri";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { IoFastFoodOutline } from "react-icons/io";
+import { BsChatDots } from "react-icons/bs";
+
 export const categories = [
-    {
-        id:1,
-        title:"Education",
-        
-    }
-]
+  {
+    id: 1,
+    title: "Education",
+    icon: <FaGraduationCap />,
+    color: "#0000FF",
+  },
+  {
+    id: 2,
+    title: "Healthcare",
+    icon: <RiMentalHealthFill />,
+    color: "00cd00",
+  },
+  {
+    id: 3,
+    title: "Food",
+    icon: <IoFastFoodOutline />,
+    color: "#ffb80e",
+  },
+  {
+    id: 4,
+    title: "Shopping",
+    icon: <AiOutlineShoppingCart />,
+    color: "#E33054",
+  },
+  {
+    id: 5,
+    title: "other",
+    icon: <BsChatDots />,
+    color: "#ff611d",
+  },
+];
